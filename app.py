bd0beb48260e9892813e54c3dbf70a58b06060e5
MetalJerk <scottlonghofer@gmail.com>
2020-10-23 23:43:31 -0400

    init

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory
+import os
+from os.path import join, dirname, realpath
+from werkzeug.utils import secure_filename
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config.from_object('config')
+UPLOAD_FOLDER = './static/uploads'
+ALLOWED_EXTENSIONS = {'mp4', 'mp3', 'wav'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        # if file != allowed_file(file.filename):
+        #     flash('file not allowed')
+            # return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return render_template('uploads.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(24)
+    app.run(debug=True)
+
