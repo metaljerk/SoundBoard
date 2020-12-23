7dca11fd3aa2dd42a79de6262981638256763d9e
MetalJerk <scottlonghofer@gmail.com>
2020-12-23 18:25:27 -0500

    added upoads

--- app.py
+++ app.py
@@ -40,9 +40,9 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('upload_file'))
+            return redirect(url_for('uploaded_file', filename=filename))
     return render_template('uploads.html')
-
+    
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
